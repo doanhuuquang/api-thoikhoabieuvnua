bd7199f52e0f71b9f69400678874e809b082c673
Doan Huu Quang <doanhuuquang.dev@gmail.com>
2025-06-11 09:15:46 +0700

    Cho phép gọi api từ localhost port 3000

--- src/app.ts
+++ src/app.ts
@@ -24,6 +24,7 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500).json({
     message: err.message || "Có lỗi xảy ra",
   });
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
 });
 
 export const viteNodeApp = app;
